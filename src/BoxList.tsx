ee9b8453dd29cf82913aa827d8e0e73e01b6a453
Dong Hyun Choi <dongandrewchoi@gmail.com>
2022-05-09 17:22:49 -0700

    origin main

---
+++ src/BoxList.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Box from "./Box";
+import NewBoxForm from "./NewBoxForm";
+
+/** Manage list of boxes
+ *
+ * State:
+ * - boxes: [ { id, width, height, backgroundColor }, ... ]
+ */
+
+interface IBox {
+  id:string;
+  width:number;
+  height:number;
+  backgroundColor:string;
+}
+
+
+
+function BoxList(){
+  const [boxes, setBoxes] = useState<IBox[]>([])
+
+  /** add box with given { id, width, height, backgroundColor } */
+  function add(newBox:IBox):void {
+    setBoxes(boxes => [...boxes, newBox]);
+  }
+
+  /** remove box matching that id. */
+  function remove(id: string):void {
+    setBoxes(boxes => boxes.filter(box => box.id !== id));
+  }
+
+  return (
+    <div>
+      <NewBoxForm createBox={add} />
+      {boxes.map(({ id, width, height, backgroundColor }) => (
+        <Box
+          key={id}
+          id={id}
+          width={width}
+          height={height}
+          remove={remove}
+          backgroundColor={backgroundColor}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default BoxList;
